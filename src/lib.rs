b301a564d6e37946c1c8ed404fdd186fc9592075
James Bell <jamesbell@microsoft.com>
2021-04-09 17:24:54 -0700

    Going to work on this more, should be able to get it up and running this weeekend

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemTrait, AttributeArgs};
+
+#[proc_macro_attribute]
+pub fn role(attr: TokenStream, input: TokenStream) -> TokenStream{
+    let og = input.clone();
+    let args = parse_macro_input!(attr as AttributeArgs);
+    let input = parse_macro_input!(input as ItemTrait);
+    og
 }
