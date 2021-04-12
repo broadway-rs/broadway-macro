eef1c1048b237fa6486c1f7a62fac230eab3ffac
James Bell <jamesbell@microsoft.com>
2021-04-12 10:48:34 -0700

    Idk

--- src/lib.rs
+++ src/lib.rs
@@ -37,5 +37,6 @@ pub fn role(attr: TokenStream, input: TokenStream) -> TokenStream{
         type Calls = Call<#call_ident, #reply_ident>;
         type MutCalls = Call<#mut_call_ident, #reply_ident>;
     };
-    TokenStream::from(quote!{#final_trait_impl { #final_trait_types }})
+    og.extend(TokenStream::from(quote!{#final_trait_impl { #final_trait_types }}));
+    og
 }
