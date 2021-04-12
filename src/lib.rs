289f18df26842c4ae0dafe2b3fbac03253161b72
James <jamescarterbell@gmail.com>
2021-04-11 22:09:35 -0700

    Pushing work done

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,22 @@ use quote::quote;
 use syn::{parse_macro_input, ItemTrait, AttributeArgs};
 
 #[proc_macro_attribute]
-pub fn role(attr: TokenStream, input: TokenStream) -> TokenStream{
+pub fn role(attr: TokenStream, input: TokenStream) -> Result<TokenStream, BroadwayMacroError>{
     let og = input.clone();
     let args = parse_macro_input!(attr as AttributeArgs);
+    let mut key = None;
+    let mut actor = None;
+    for arg in arg.iter(){
+        if let syn::NestedMeta::Meta(meta) = arg{
+            if let sync::Meta::NameValue(name_value) = meta{
+                
+            }
+        }
+    }
     let input = parse_macro_input!(input as ItemTrait);
-    og
+}
+
+pub enum BroadwayMacroError{
+    MissingKey,
+    MissingActor,
 }
