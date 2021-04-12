213314b5c945a2b11bbfcf97aba0e8156703f903
James Bell <jamesbell@microsoft.com>
2021-04-12 10:12:52 -0700

    macro experimenting

--- src/lib.rs
+++ src/lib.rs
@@ -41,5 +41,5 @@ pub fn role(attr: TokenStream, input: TokenStream) -> TokenStream{
         type Calls = Call<#call_ident, #reply_ident>;
         type MutCalls = Call<#mut_call_ident, #reply_ident>;
     };
-    (quote!{#final_trait_impl { #final_trait_types }}).into()
+    (quote!{#og #final_trait_impl { #final_trait_types }}).into()
 }
